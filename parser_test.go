2fe290e6243a76073a6c0ca3a96e99144c35d514
Yuya Kono <takohati0821@gmail.com>
2023-10-18 06:38:00 +0000

    fuzzing test

--- parser_test.go
+++ parser_test.go
@@ -3,6 +3,7 @@ package main_test
 import (
 	"strings"
 	"testing"
+	"unicode/utf8"
 
 	. "github.com/takoeight0821/anma"
 )
@@ -25,34 +26,43 @@ func completeParseExpr(t *testing.T, input string, expected string) {
 	}
 }
 
+var testcases = []struct {
+	input    string
+	expected string
+}{
+	{"1", "(literal 1)"},
+	{`"hello"`, `(literal "hello")`},
+	{"f()", "(call (var f))"},
+	{"f(1)", "(call (var f) (literal 1))"},
+	{"f(1, 2)", "(call (var f) (literal 1) (literal 2))"},
+	{"f(1)(2)", "(call (call (var f) (literal 1)) (literal 2))"},
+	{"f(1,)", "(call (var f) (literal 1))"},
+	{"a.b", "(access (var a) b)"},
+	{"a.b.c", "(access (access (var a) b) c)"},
+	{"f(x) + g(y).z", "(binary (call (var f) (var x)) + (access (call (var g) (var y)) z))"},
+	{"x : Int", "(assert (var x) (var Int))"},
+	{"let x = 1", "(let (var x) (literal 1))"},
+	{"let x = 1 : Int", "(let (var x) (assert (literal 1) (var Int)))"},
+	{"let Cons(x, xs) = list", "(let (call (var Cons) (var x) (var xs)) (var list))"},
+	{"{ #(x, y) -> x + y }", "(codata (clause (call (this #) (var x) (var y)) (binary (var x) + (var y))))"},
+	{"{ #(x, y) -> x + y; x }", "(codata (clause (call (this #) (var x) (var y)) (binary (var x) + (var y)) (var x)))"},
+	{"{ #(x, y) -> x + y; x; }", "(codata (clause (call (this #) (var x) (var y)) (binary (var x) + (var y)) (var x)))"},
+	{"{ #(x, y) -> x + y, #(x, y) -> x - y }", "(codata (clause (call (this #) (var x) (var y)) (binary (var x) + (var y))) (clause (call (this #) (var x) (var y)) (binary (var x) - (var y))))"},
+	{"{ #(x, y) -> x + y, #(x, y) -> x - y, }", "(codata (clause (call (this #) (var x) (var y)) (binary (var x) + (var y))) (clause (call (this #) (var x) (var y)) (binary (var x) - (var y))))"},
+	{"fn x { x + 1 }", "(lambda (var x) (binary (var x) + (literal 1)))"},
+	{"(x, y, z)", "(paren (var x) (var y) (var z))"},
+	{"(x, y, z,)", "(paren (var x) (var y) (var z))"},
+	{"()", "(paren)"},
+	{"f : a -> b", "(assert (var f) (binary (var a) -> (var b)))"},
+	{"fn x { let y = 1; x + y }", "(lambda (var x) (let (var y) (literal 1)) (binary (var x) + (var y)))"},
+	{"fn x { let y = 1; x + y; }", "(lambda (var x) (let (var y) (literal 1)) (binary (var x) + (var y)))"},
+	{"{ #.head -> 1 }", "(codata (clause (access (this #) head) (literal 1)))"},
+}
+
 func TestParse(t *testing.T) {
-	completeParseExpr(t, "1", "(literal 1)")
-	completeParseExpr(t, `"hello"`, `(literal "hello")`)
-	completeParseExpr(t, "f()", "(call (var f))")
-	completeParseExpr(t, "f(1)", "(call (var f) (literal 1))")
-	completeParseExpr(t, "f(1, 2)", "(call (var f) (literal 1) (literal 2))")
-	completeParseExpr(t, "f(1)(2)", "(call (call (var f) (literal 1)) (literal 2))")
-	completeParseExpr(t, "f(1,)", "(call (var f) (literal 1))")
-	completeParseExpr(t, "a.b", "(access (var a) b)")
-	completeParseExpr(t, "a.b.c", "(access (access (var a) b) c)")
-	completeParseExpr(t, "f(x) + g(y).z", "(binary (call (var f) (var x)) + (access (call (var g) (var y)) z))")
-	completeParseExpr(t, "x : Int", "(assert (var x) (var Int))")
-	completeParseExpr(t, "let x = 1", "(let (var x) (literal 1))")
-	completeParseExpr(t, "let x = 1 : Int", "(let (var x) (assert (literal 1) (var Int)))")
-	completeParseExpr(t, "let Cons(x, xs) = list", "(let (call (var Cons) (var x) (var xs)) (var list))")
-	completeParseExpr(t, "{ #(x, y) -> x + y }", "(codata (clause (call (this #) (var x) (var y)) (binary (var x) + (var y))))")
-	completeParseExpr(t, "{ #(x, y) -> x + y; x }", "(codata (clause (call (this #) (var x) (var y)) (binary (var x) + (var y)) (var x)))")
-	completeParseExpr(t, "{ #(x, y) -> x + y; x; }", "(codata (clause (call (this #) (var x) (var y)) (binary (var x) + (var y)) (var x)))")
-	completeParseExpr(t, "{ #(x, y) -> x + y, #(x, y) -> x - y }", "(codata (clause (call (this #) (var x) (var y)) (binary (var x) + (var y))) (clause (call (this #) (var x) (var y)) (binary (var x) - (var y))))")
-	completeParseExpr(t, "{ #(x, y) -> x + y, #(x, y) -> x - y, }", "(codata (clause (call (this #) (var x) (var y)) (binary (var x) + (var y))) (clause (call (this #) (var x) (var y)) (binary (var x) - (var y))))")
-	completeParseExpr(t, "fn x { x + 1 }", "(lambda (var x) (binary (var x) + (literal 1)))")
-	completeParseExpr(t, "(x, y, z)", "(paren (var x) (var y) (var z))")
-	completeParseExpr(t, "(x, y, z,)", "(paren (var x) (var y) (var z))")
-	completeParseExpr(t, "()", "(paren)")
-	completeParseExpr(t, "f : a -> b", "(assert (var f) (binary (var a) -> (var b)))")
-	completeParseExpr(t, "fn x { let y = 1; x + y }", "(lambda (var x) (let (var y) (literal 1)) (binary (var x) + (var y)))")
-	completeParseExpr(t, "fn x { let y = 1; x + y; }", "(lambda (var x) (let (var y) (literal 1)) (binary (var x) + (var y)))")
-	completeParseExpr(t, "{ #.head -> 1 }", "(codata (clause (access (this #) head) (literal 1)))")
+	for _, testcase := range testcases {
+		completeParseExpr(t, testcase.input, testcase.expected)
+	}
 }
 
 func completeParseDecl(t *testing.T, input string, expected string) {
@@ -79,7 +89,56 @@ func completeParseDecl(t *testing.T, input string, expected string) {
 	}
 }
 
+var testcasesDecl = []struct {
+	input    string
+	expected string
+}{
+	{"def x = 1", "(def x (literal 1))\n"},
+	{"type List = { head: Int, tail: List }", "(type List (object (field head (var Int)) (field tail (var List))))\n"},
+}
+
 func TestParseDecl(t *testing.T) {
-	completeParseDecl(t, "def x = 1", "(def x (literal 1))\n")
-	completeParseDecl(t, "type List = { head: Int, tail: List }", "(type List (object (field head (var Int)) (field tail (var List))))\n")
+	for _, testcase := range testcasesDecl {
+		completeParseDecl(t, testcase.input, testcase.expected)
+	}
+}
+
+func tryParse(t *testing.T, input string) {
+	tokens, err := Lex(input)
+	if err != nil {
+		t.Logf("Lex(%q) returned error: %v", input, err)
+		return
+	}
+	t.Logf("tokens: %v", tokens)
+
+	p := NewParser(tokens)
+	_, err = p.ParseExpr()
+	if err != nil {
+		t.Logf("Parse(%q) returned error: %v", input, err)
+		return
+	}
+
+	p = NewParser(tokens)
+	_, err = p.ParseDecl()
+	if err != nil {
+		t.Logf("Parse(%q) returned error: %v", input, err)
+		return
+	}
+}
+
+// parseExpr must stop for any random string.
+// go test -fuzz=Fuzz -fuzztime 1000x
+func FuzzParseExpr(f *testing.F) {
+	for _, testcase := range testcases {
+		f.Add(testcase.input)
+	}
+	for _, testcase := range testcasesDecl {
+		f.Add(testcase.input)
+	}
+	f.Fuzz(func(t *testing.T, input string) {
+		if utf8.ValidString(input) {
+			t.Logf("input: %q", input)
+			tryParse(t, input)
+		}
+	})
 }
