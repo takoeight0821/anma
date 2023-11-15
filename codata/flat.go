894dd68fa5d3fe272e633a4710d5c041114a8129
Yuya Kono <takohati0821@gmail.com>
2023-11-15 03:27:55 +0000

    reduce unnecessary case expressions

--- codata/flat.go
+++ codata/flat.go
@@ -45,15 +45,15 @@ const (
 )
 
 type ArityError struct {
-	Expected int
+	Expected int // expected arity, or notChecked, or noArgs
 	Where    token.Token
 }
 
 func (e ArityError) Error() string {
-	if e.Expected >= noArgs {
-		return utils.MsgAt(e.Where, fmt.Sprintf("arity mismatch: expected %d arguments", e.Expected))
+	if e.Expected == notChecked {
+		return utils.MsgAt(e.Where, fmt.Sprintf("unreachable: arity is not checked"))
 	}
-	return utils.MsgAt(e.Where, fmt.Sprintf("unreachable: arity is not checked"))
+	return utils.MsgAt(e.Where, fmt.Sprintf("arity mismatch: expected %d arguments", e.Expected))
 }
 
 func flatCodata(c *ast.Codata) ast.Node {
@@ -150,7 +150,7 @@ func (b builder) object(clauses []*ast.Clause) ast.Node {
 			// if c has no accessors, generate pattern matching clause
 			plist := c.Pattern.(patternList)
 			if len(plist.accessors) == 0 {
-				caseClauses[i] = plistToClause(c.Pattern, c.Exprs...)
+				caseClauses[i] = plistToClause(c.Pattern.(patternList), c.Exprs...)
 			} else {
 				// otherwise, add to restClauses
 				restClauses[i] = c
@@ -164,34 +164,17 @@ func (b builder) object(clauses []*ast.Clause) ast.Node {
 		})
 
 		for i, c := range restClauses {
-			caseClauses[i] = plistToClause(c.Pattern, b.object(restClausesList))
+			caseClauses[i] = plistToClause(c.Pattern.(patternList), b.object(restClausesList))
 		}
 
 		fields = append(fields,
 			&ast.Field{
 				Name:  field,
-				Exprs: newCaseField(b.scrutinees, caseClauses),
+				Exprs: newCase(b.scrutinees, caseClauses),
 			})
 		return
 	})
 	return &ast.Object{Fields: fields}
-}
-
-func plistToClause(pattern ast.Node, exprs ...ast.Node) *ast.Clause {
-	plist, ok := pattern.(patternList)
-	if !ok {
-		panic(UnsupportedPatternError{Clause: &ast.Clause{Pattern: pattern, Exprs: exprs}})
-	}
-	return &ast.Clause{Pattern: &ast.Paren{Elems: plist.params}, Exprs: exprs}
-}
-
-func newCaseField(scrs []ast.Node, cs []*ast.Clause) []ast.Node {
-	// if there is no scrutinee, return Exprs of the first clause
-	// because case expression always matches the first clause.
-	if len(scrs) == 0 {
-		return cs[0].Exprs
-	}
-	return []ast.Node{&ast.Case{Scrutinee: &ast.Paren{Elems: scrs}, Clauses: cs}}
 }
 
 // Generate lambda and dispatch body expression to Object or Case based on existence of accessors.
@@ -206,30 +189,48 @@ func (b *builder) lambda(arity int, clauses []*ast.Clause) ast.Node {
 	// If any of clauses has accessors, body expression is Object.
 	for _, c := range clauses {
 		if len(c.Pattern.(patternList).accessors) != 0 {
-			return &ast.Lambda{Pattern: &ast.Paren{Elems: b.scrutinees}, Exprs: []ast.Node{b.object(clauses)}}
+			return newLambda(b.scrutinees, b.object(clauses))
 		}
 	}
 
 	// otherwise, body expression is Case.
 	caseClauses := make([]*ast.Clause, 0)
 	for _, c := range clauses {
-		plist := c.Pattern.(patternList)
-		caseClauses = append(caseClauses, &ast.Clause{Pattern: &ast.Paren{Elems: plist.params}, Exprs: c.Exprs})
+		caseClauses = append(caseClauses, plistToClause(c.Pattern.(patternList), c.Exprs...))
 	}
-	return &ast.Lambda{
-		Pattern: &ast.Paren{Elems: b.scrutinees},
-		Exprs: []ast.Node{
-			&ast.Case{
-				Scrutinee: &ast.Paren{Elems: b.scrutinees},
-				Clauses:   caseClauses,
-			},
-		},
+	return newLambda(b.scrutinees, newCase(b.scrutinees, caseClauses)...)
+}
+
+// newLambda creates a new Lambda node with the given parameters and expressions.
+// If there is only one parameter, return it without Paren pattern.
+// Otherwise, parameters are wrapped by Paren pattern.
+func newLambda(params []ast.Node, exprs ...ast.Node) *ast.Lambda {
+	if len(params) == 1 {
+		return &ast.Lambda{Pattern: params[0], Exprs: exprs}
 	}
+	return &ast.Lambda{Pattern: &ast.Paren{Elems: params}, Exprs: exprs}
 }
 
 // newVar creates a new Var node with the given name and a token.
 func newVar(name string, base token.Token) *ast.Var {
 	return &ast.Var{Name: token.Token{Kind: token.IDENT, Lexeme: name, Line: base.Line, Literal: nil}}
+}
+
+// plistToClause creates a new Clause node with the given pattern and expressions.
+// pattern must be a patternList.
+func plistToClause(plist patternList, exprs ...ast.Node) *ast.Clause {
+	return &ast.Clause{Pattern: &ast.Paren{Elems: plist.params}, Exprs: exprs}
+}
+
+// newCase creates a new Case node with the given scrutinees and clauses.
+// If there is no scrutinee, return Exprs of the first clause.
+func newCase(scrs []ast.Node, cs []*ast.Clause) []ast.Node {
+	// if there is no scrutinee, return Exprs of the first clause
+	// because case expression always matches the first clause.
+	if len(scrs) == 0 {
+		return cs[0].Exprs
+	}
+	return []ast.Node{&ast.Case{Scrutinee: &ast.Paren{Elems: scrs}, Clauses: cs}}
 }
 
 type InvalidCallPatternError struct {
@@ -271,33 +272,27 @@ type patternList struct {
 }
 
 func (p patternList) Base() token.Token {
-	if len(p.accessors) == 0 {
-		if len(p.params) == 0 {
-			return token.Token{}
-		}
+	if len(p.accessors) != 0 {
+		return p.accessors[0]
+	}
+	if len(p.params) != 0 {
 		return p.params[0].Base()
 	}
-	return p.accessors[0]
+	return token.Token{}
 }
 
 func (p patternList) String() string {
-	var b strings.Builder
-	b.WriteString("[")
+	accessors := make([]string, len(p.accessors))
 	for i, a := range p.accessors {
-		if i > 0 {
-			b.WriteString(" ")
-		}
-		b.WriteString(a.String())
+		accessors[i] = a.String()
 	}
-	b.WriteString(" | ")
+
+	params := make([]string, len(p.params))
 	for i, p := range p.params {
-		if i > 0 {
-			b.WriteString(" ")
-		}
-		b.WriteString(p.String())
+		params[i] = p.String()
 	}
-	b.WriteString("]")
-	return b.String()
+
+	return "[" + strings.Join(accessors, " ") + " | " + strings.Join(params, " ") + "]"
 }
 
 var _ ast.Node = patternList{}
