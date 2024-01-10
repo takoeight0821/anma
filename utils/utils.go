ba738dac0d0cf827f5f601a1e1439f01837a7000
Yuya Kono <takohati0821@gmail.com>
2024-01-10 08:34:18 +0000

    Remove unused imports and functions

--- utils/utils.go
+++ utils/utils.go
@@ -4,30 +4,8 @@ import (
 	"fmt"
 
 	"github.com/takoeight0821/anma/token"
-	"golang.org/x/exp/constraints"
-	"golang.org/x/exp/slices"
 	"gopkg.in/yaml.v3"
 )
-
-func All[T any](slice []T, pred func(T) bool) bool {
-	for _, v := range slice {
-		if !pred(v) {
-			return false
-		}
-	}
-	return true
-}
-
-func OrderedFor[I constraints.Ordered, V any](m map[I]V, f func(I, V)) {
-	keys := make([]I, 0, len(m))
-	for k := range m {
-		keys = append(keys, k)
-	}
-	slices.Sort(keys)
-	for _, k := range keys {
-		f(k, m[k])
-	}
-}
 
 type ErrorAt struct {
 	Where token.Token
